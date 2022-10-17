38c8b08fb01d304ac528e31bf6fe690b9b3c1ed2
Dolf Barr <mail@dolf.me>
2022-10-17 22:21:33 +0400

    Move to gatsby

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import type { HeadFC } from "gatsby"
+
+const IndexPage = () => {
+  return (
+    <main>
+      dolf.me
+    </main>
+  )
+}
+
+export default IndexPage
+
+export const Head: HeadFC = () => <title>Home Page</title>
