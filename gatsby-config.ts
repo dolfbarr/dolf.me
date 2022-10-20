fdffa2c83b246f94eff145b8bd0369aa426de8f6
Dolf Barr <mail@dolf.me>
2022-10-20 20:49:06 +0400

    Add tailwind

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,26 +9,35 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ['gatsby-plugin-image', {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      icon: 'src/images/icon.png'
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-image',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png'
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: './src/images/'
+      },
+      __key: 'images'
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/'
+      },
+      __key: 'pages'
     }
-  }, 'gatsby-plugin-mdx', 'gatsby-plugin-sharp', 'gatsby-transformer-sharp', {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      name: 'images',
-      path: './src/images/'
-    },
-    __key: 'images'
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      name: 'pages',
-      path: './src/pages/'
-    },
-    __key: 'pages'
-  }]
+  ]
 }
 
 export default config
