56b05455ac354a0ae551d0018f285dbf08ef11a3
Dolf Barr <mail@dolf.me>
2022-10-21 15:36:19 +0400

    Add headings styling

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -5,8 +5,13 @@ interface HeadingProps {
 }
 
 const Heading = ({ children, id }: HeadingProps): React.FC => (
-  <h2 className="text-5xl text-sky-600 " id={id}>
-    <a href={`#${id}`}>{children}</a>
+  <h2 className="group text-5xl text-sky-600" id={id}>
+    <a href={`#${id}`} className="-ml-8 hover:underline">
+      <span className="inline-block w-8 opacity-0 transition-opacity group-hover:opacity-100">
+        #
+      </span>
+      <span>{children}</span>
+    </a>
   </h2>
 )
 
