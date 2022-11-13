ad087ea7e0a1a008edbbe47e4089f6e7d776581b
Dolf Barr <mail@dolf.me>
2022-11-13 17:22:17 +0100

    Fix reset theme

--- src/contexts/ThemeModeProvider.tsx
+++ src/contexts/ThemeModeProvider.tsx
@@ -52,6 +52,11 @@ const ThemeModeProvider: React.FC<PropsWithChildren> = ({
       localStorage.theme = ''
     }
 
+    setDarkTheme(
+      typeof window !== 'undefined'
+        ? window.matchMedia('(prefers-color-scheme: dark)').matches
+        : false,
+    )
     setIsThemeReset(true)
   }
 
