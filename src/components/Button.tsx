814d1118c46f4ed7f720a868139f310b31b350a4
Dolf Barr <mail@dolf.me>
2022-11-13 17:02:13 +0100

    Fix button focus

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ const Button: React.FC<
 > = ({ children, className, ...props }): ReactElement => (
   <button
     className={classNames(
-      'rounded border border-primary bg-transparent px-2 text-primary transition-colors hover:border-transparent hover:bg-primary hover:text-white active:brightness-90 dark:border-primary-dark dark:text-primary-dark dark:hover:bg-primary-dark dark:hover:hover:text-white',
+      'rounded border border-primary bg-transparent px-2 text-primary transition-colors hover:border-transparent hover:bg-primary hover:text-white focus:border-transparent focus:bg-primary focus:text-white active:brightness-90 dark:border-primary-dark dark:text-primary-dark dark:hover:bg-primary-dark dark:hover:text-white dark:focus:bg-primary-dark dark:focus:text-white',
       className,
     )}
     {...props}>
